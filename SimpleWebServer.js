fe7ebff4e2a0ac520e1b2a404fe2e87e479beabf
Josue Kpodo <josuk@pm.me>
2018-12-30 22:06:21 -0500

    Update SimpleWebServer.js

--- SimpleWebServer.js
+++ SimpleWebServer.js
@@ -4,8 +4,9 @@ var http = require('http');  // require the http module
 var server = http.createServer(
     function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello from the BeagleBone Black!\n');
+        res.end('Hola from BBB!\n');
     });
 server.listen(5050);
 console.log('BBB Web Server running at http://192.168.7.2:5050/');
+console.log('All running well so far...\n');
 
